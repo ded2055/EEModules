3a67aa3fe0b577329cc7fb50f4bda9f7b6f1c6e4
ded2055 <ded2055@yandex.ru>
2016-06-16 11:10:21 +0300

    rename NewClass -> SquareSum

--- src/main/java/Module3/Module3_2/NewClass.java
+++
@@ -1,7 +0,0 @@
-package Module3.Module3_2;
-
-/**
- * Created by Ded on 16.06.2016.
- */
-public class NewClass {
-}
---
+++ src/main/java/Module3/Module3_2/SquareSum.java
@@ -0,0 +1,7 @@
+package Module3.Module3_2;
+
+interface SquareSum {
+
+    long getSquareSum(int[] values, int numberOfThreads);
+
+}
